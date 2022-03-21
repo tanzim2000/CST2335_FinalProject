cffabb9d0b3a3d98fc95060f095250d42a91f816
Truby <trubyzhao@gmail.com>
2022-03-21 15:56:04 -0400

    finalProject-Truby

--- app/src/main/java/com/cst2335/cst2335_finalproject/DetailsFragment.java
+++ app/src/main/java/com/cst2335/cst2335_finalproject/DetailsFragment.java
@@ -1,64 +1,76 @@
 package com.cst2335.cst2335_finalproject;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.TextView;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link DetailsFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
 public class DetailsFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    int position;
+    String eventName;
+    String eventURL;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    //Bundle args;
+    TextView showName;
+    TextView showURL;
+    Button btnHide;
+    DetailsFragment df;
 
     public DetailsFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment DetailsFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static DetailsFragment newInstance(String param1, String param2) {
-        DetailsFragment fragment = new DetailsFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
+        //getArguments from Bundle of ChartRoom
+
+        position = getArguments().getInt("position", 0);
+        eventName = getArguments().getString("eventName", "");
+        eventURL = getArguments().getString("eventUrl", "");
+
+    }
+    @Override
+    public View onCreateView(LayoutInflater inflater,  ViewGroup parent,
+                             Bundle savedInstanceState) {
+
+        // Inflate the xml file for the fragment
+        return inflater.inflate(R.layout.fragment_details, parent, false);
     }
 
+    @SuppressLint("SetTextI18n")
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_details2, container, false);
-    }
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+
+        //getArgument from CharRoom to Fragment
+
+
+        showName = view.findViewById(R.id.textView);
+        showName.setText("Event Name: " + eventName);
+        showURL = view.findViewById(R.id.textView3);
+        showURL.setText("Details Information at: " + eventURL);
+
+
+        /*
+        //click Hide button to remove the transaction
+        btnHide = view.findViewById(R.id.hide);
+        FragmentManager fm = getFragmentManager();
+        btnHide.setOnClickListener( click -> fm.beginTransaction().remove(this).commit());
+        */
+        }
+
 }
